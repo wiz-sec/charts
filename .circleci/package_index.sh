592da574a994f0c0e1a858e3786779382b0ea932
barmagnezi <bar.magnezi@wiz.io>
2023-07-04 17:47:58 +0300

    Fix helm dependency update to run in package folder (#141)

--- .circleci/package_index.sh
+++ .circleci/package_index.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 PACKAGE_VERSION=$(helm show chart ${PACKAGE} | grep version | cut -d " " -f 2)
 PACKAGE_FULL_NAME="${PACKAGE}-${PACKAGE_VERSION}.tgz"
@@ -6,7 +7,7 @@ git config user.email "circleci@wiz.io"
 git config user.name "CircleCI"
 
 # Update package dependencies
-helm dependency update
+helm dependency update $PACKAGE
 
 # Package the chart with diffs
 helm package $PACKAGE
