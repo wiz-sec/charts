ed8dc71e3e4aaa5a7c983f839c9ab8cba4ebd58b
Lior Schachter <lior.schachter@wiz.io>
2024-11-07 17:46:49 +0200

    fixes

--- .circleci/tests/golden.go
+++ .circleci/tests/golden.go
@@ -14,13 +14,14 @@ import (
 var update = flag.Bool("update-golden", false, "update golden test output files")
 
 type TemplateGoldenTest struct {
-	ChartPath      string
-	Release        string
-	Namespace      string
-	GoldenFileName string
-	Templates      []string
-	SetValues      map[string]string
-	ValuesFile     string
+	ChartPath          string
+	Release            string
+	Namespace          string
+	GoldenFileName     string
+	Templates          []string
+	SetValues          map[string]string
+	ValuesFile         string
+	GoldenSubDirectory string
 }
 
 // TestContainerGoldenTestDefaults Code is based on the article here:  https://medium.com/@zelldon91/advanced-test-practices-for-helm-charts-587caeeb4cb
@@ -38,7 +39,7 @@ func TestContainerGoldenTestDefaults(t *testing.T, testCase *TemplateGoldenTest)
 	bytes := regex.ReplaceAll([]byte(output), []byte(""))
 	output = string(bytes)
 
-	goldenFile := "golden/" + testCase.GoldenFileName + ".golden.yaml"
+	goldenFile := "golden/" + testCase.GoldenSubDirectory + "/" + testCase.GoldenFileName + ".golden.yaml"
 
 	if *update {
 		err := os.WriteFile(goldenFile, bytes, 0644)
--- .circleci/tests/test1_test.go
+++ .circleci/tests/test1_test.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -18,37 +19,66 @@ func TestSs(t *testing.T) {
 	suite.Run(t, new(ss))
 }
 
-var supportedHelmCharts = []string{
-	"wiz-sensor",
-	"wiz-admission-controller",
-	"wiz-kubernetes-connector",
-	"wiz-kubernetes-integration",
-}
-
 func (s *ss) TestChartTemplateWithDefaultValues() {
-	for _, chartName := range supportedHelmCharts {
+	for _, chartName := range []string{
+		"wiz-sensor",
+		"wiz-admission-controller",
+		"wiz-kubernetes-connector",
+		"wiz-kubernetes-integration",
+	} {
 		s.Run(chartName, func() {
-			chartDir := path.Join(chartsRootDir, chartName)
-			chartFilePath := path.Join(chartDir, "Chart.yaml")
-			if _, err := os.Stat(chartFilePath); os.IsNotExist(err) {
-				//	fail the test
-				s.Fail("Chart.yaml file not found in %s", chartDir)
-			}
-
-			_, err := chartutil.IsChartDir(chartDir)
-			s.NoError(err)
-
+			chartDir := s.getChartDirectory(chartName)
 			chartDirFullPath, err := filepath.Abs(chartDir)
 			s.NoError(err)
 			defaultValuesFilePath := path.Join(chartDir, "values.yaml")
 
 			TestContainerGoldenTestDefaults(s.T(), &TemplateGoldenTest{
-				ChartPath:      chartDirFullPath,
-				Release:        "release-test",
-				Namespace:      "release-helm-namespace",
-				GoldenFileName: chartName,
-				ValuesFile:     defaultValuesFilePath,
+				ChartPath:          chartDirFullPath,
+				Release:            "release-test",
+				Namespace:          "release-helm-namespace",
+				GoldenFileName:     chartName,
+				ValuesFile:         defaultValuesFilePath,
+				GoldenSubDirectory: "default",
 			})
 		})
 	}
+}
+
+func (s *ss) TestChartTemplatesWithCustomValues() {
+	testFiles, err := os.ReadDir("testfiles")
+	s.NoError(err)
+
+	for _, testFile := range testFiles {
+		s.Run(testFile.Name(), func() {
+			chartName := strings.Split(strings.Split(testFile.Name(), ".")[0], "_")[0]
+			chartDir := s.getChartDirectory(chartName)
+
+			chartDirFullPath, err := filepath.Abs(chartDir)
+			s.NoError(err)
+
+			valuesFilePath := path.Join("testfiles", testFile.Name())
+			TestContainerGoldenTestDefaults(s.T(), &TemplateGoldenTest{
+				ChartPath:          chartDirFullPath,
+				Release:            "release-test",
+				Namespace:          "release-helm-namespace",
+				GoldenFileName:     chartName,
+				ValuesFile:         valuesFilePath,
+				GoldenSubDirectory: "custom",
+			})
+		})
+	}
+}
+
+func (s *ss) getChartDirectory(chartName string) string {
+	chartDir := path.Join(chartsRootDir, chartName)
+	if _, err := os.Stat(path.Join(chartDir, "Chart.yaml")); os.IsNotExist(err) {
+		//	fail the test
+		s.Fail("Chart.yaml file not found in %s", chartDir)
+	}
+
+	isChartDir, err := chartutil.IsChartDir(chartDir)
+	s.NoError(err)
+	s.True(isChartDir)
+
+	return chartDir
 }
