fab7aa65ecb2c7d09f8628ad28ba7609af28c483
Lior Schachter <lior.schachter@wiz.io>
2024-11-07 16:59:30 +0200

    wip

---
+++ .circleci/tests/golden.go
@@ -0,0 +1,53 @@
+package tests
+
+import (
+	"flag"
+	"os"
+	"regexp"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/stretchr/testify/require"
+)
+
+var update = flag.Bool("update-golden", false, "update golden test output files")
+
+type TemplateGoldenTest struct {
+	ChartPath      string
+	Release        string
+	Namespace      string
+	GoldenFileName string
+	Templates      []string
+	SetValues      map[string]string
+	ValuesFile     string
+}
+
+// TestContainerGoldenTestDefaults Code is based on the article here:  https://medium.com/@zelldon91/advanced-test-practices-for-helm-charts-587caeeb4cb
+func TestContainerGoldenTestDefaults(t *testing.T, testCase *TemplateGoldenTest) {
+	r := require.New(t)
+
+	options := &helm.Options{
+		KubectlOptions:    k8s.NewKubectlOptions("", "", testCase.Namespace),
+		SetValues:         testCase.SetValues,
+		ValuesFiles:       []string{testCase.ValuesFile},
+		BuildDependencies: false,
+	}
+	output := helm.RenderTemplate(t, options, testCase.ChartPath, testCase.Release, testCase.Templates)
+	regex := regexp.MustCompile(`\s+helm.sh/chart:\s+.*`)
+	bytes := regex.ReplaceAll([]byte(output), []byte(""))
+	output = string(bytes)
+
+	goldenFile := "golden/" + testCase.GoldenFileName + ".golden.yaml"
+
+	if *update {
+		err := os.WriteFile(goldenFile, bytes, 0644)
+		r.NoError(err, "Golden file was not writable")
+	}
+
+	expected, err := os.ReadFile(goldenFile)
+
+	// then
+	r.NoError(err, "Golden file doesn't exist or was not readable")
+	r.Equal(string(expected), output)
+}
---
+++ .circleci/tests/test1_test.go
@@ -0,0 +1,56 @@
+package tests
+
+import (
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/stretchr/testify/suite"
+	"helm.sh/helm/v3/pkg/chartutil"
+)
+
+type ss struct {
+	suite.Suite
+}
+
+func TestSs(t *testing.T) {
+	suite.Run(t, new(ss))
+}
+
+var supportedHelmCharts = []string{
+	"wiz-sensor",
+	"wiz-admission-controller",
+	"wiz-kubernetes-connector",
+	"wiz-kubernetes-integration",
+}
+
+func (s *ss) TestChartTemplateWithDefaultValues() {
+	for _, chartName := range supportedHelmCharts {
+		s.Run(chartName, func() {
+			chartDir := path.Join(chartsRootDir, chartName)
+			chartFilePath := path.Join(chartDir, "Chart.yaml")
+			if _, err := os.Stat(chartFilePath); os.IsNotExist(err) {
+				//	fail the test
+				s.Fail("Chart.yaml file not found in %s", chartDir)
+			}
+
+			_, err := chartutil.IsChartDir(chartDir)
+			s.NoError(err)
+
+			chartDirFullPath, err := filepath.Abs(chartDir)
+			s.NoError(err)
+			defaultValuesFilePath := path.Join(chartDir, "values.yaml")
+
+			TestContainerGoldenTestDefaults(s.T(), &TemplateGoldenTest{
+				ChartPath:      chartDirFullPath,
+				Release:        "release-test",
+				Namespace:      "release-helm-" + strings.ToLower(random.UniqueId()),
+				GoldenFileName: chartName,
+				ValuesFile:     defaultValuesFilePath,
+			})
+		})
+	}
+}
